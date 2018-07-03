1b1c12427179ad4c032febbbdf2461b1e6a763b1
Renyx1994 <renyixiong1994@gmail.com>
2018-07-03 18:39:06 -0500

    Display Database Data

--- NBA_plus/views.py
+++ NBA_plus/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from NBA_plus.models import Nba
 
 # Create your views here.
 def index(request):
     return render(request, 'NBA_plus/index.html')
+
+def test(request):
+    nbadata = Nba.objects.all()
+    return render(request, 'NBA_plus/test.html', {'nbadata': nbadata})
