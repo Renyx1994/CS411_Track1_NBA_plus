5250ac9fe25790fcc2750a94f8d975dd386ee662
Renyx1994 <renyixiong1994@gmail.com>
2018-07-17 23:02:18 -0500

    Delete records

--- NBA_plus/urls.py
+++ NBA_plus/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
     path('player/', views.player, name='player'),
-    path('delplayer/<player_id>/', views.delplayer, name='del_player'),
+    path('delplayer/<pid>/', views.delplayer, name='del_player'),
 ]
