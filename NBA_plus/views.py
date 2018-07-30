731c5685b870cf5f3da8820ff7aa040b2c76341a
Renyx1994 <renyixiong1994@gmail.com>
2018-07-30 01:49:51 -0500

    Similarity

--- NBA_plus/views.py
+++ NBA_plus/views.py
@@ -66,9 +66,10 @@ def updateplayer(request,pid):
 
 def similarplayer(request):
     simform = SimilarplayerForm(request.POST)
+    sim = ''
     if simform.is_valid():
         name = simform.cleaned_data['post']
-    sim = PlayerSimilarity.objects.get(name__icontains=name)
+        sim = PlayerSimilarity.objects.get(name__icontains=name)
     return render(request, 'NBA_plus/similarity.html', {'simform':simform,'sim':sim})
 
 def delteam(request,tid):
